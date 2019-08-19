eb06a66765b07c2ca713b1dc0b5ba6d48f9a6a66
Joseph Andy Feidje <ingandy75@gmail.com>
2019-08-19 14:00:05 -0400

    Inicial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+  get "/login" do
+    erb :login
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+end
